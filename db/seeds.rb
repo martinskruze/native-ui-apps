89431a9cd3205c0491af2fa2f06c1073d89ae5b6
Mārtiņš Krūze <martins.kruze@gmail.com>
2024-01-06 20:50:41 +0200

    initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
-# Just some data for initial manual testing
+$LOAD_PATH.unshift File.expand_path('..', __dir__)
+require 'bundler/setup'
+require 'app/application'
 
 wday_stock = Stock.create!(ticker: 'WDAY', name: 'Workday inc')
+Purchase.create!(
+  stock: wday_stock,
+  execution_time: DateTime.current.beginning_of_year,
+  quantity: 5.0,
+  price: 155.3221
+)
+Purchase.create!(
+  stock: wday_stock,
+  execution_time: DateTime.current.beginning_of_year + 5.days,
+  quantity: 2.0,
+  price: 175.3112
+)
+Sell.create!(
+  stock: wday_stock,
+  execution_time: DateTime.current.beginning_of_month,
+  quantity: 3.0,
+  price: 278.321
+)
+Sell.create!(
+  stock: wday_stock,
+  execution_time: DateTime.current.beginning_of_month + 10.days,
+  quantity: 5.0,
+  price: 302.9932
+)
