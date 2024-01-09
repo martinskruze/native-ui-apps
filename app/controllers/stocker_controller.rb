9c0cb06e9076af44a34908b70ea08c0b51ca9621
Mārtiņš Krūze <martins.kruze@gmail.com>
2024-01-09 18:29:13 +0200

    save on stocker table

--- app/controllers/stocker_controller.rb
+++ app/controllers/stocker_controller.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 class StockerController
+  COLUMN_DATA = %i[ticker name]
   def self.run
     window("Stocker", 400, 400) {
       table(:editable) {
         text_column('Ticker')
         text_column('Name')
 
-        cell_rows Stock.pluck(:ticker, :name)
+        editable true
+        cell_rows Stock.all
+
+        on_edited do |row, stock|
+          stock.save!
+        end
       }
     }.show
   end
